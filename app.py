d93a80ff021fb2c60d3bd2551d12429e389d6bf6
yigittacarr <acarryigitt@gmail.com>
2025-08-22 15:53:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Merhaba!</h1><p>Bu benim ilk Python web sitem 🎉</p>"
+    return "<h1>Merhaba Render!</h1><p>Python Flask web sitem yayında 🎉</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render'ın verdiği PORT
+    app.run(host="0.0.0.0", port=port)        # Tüm ağ arayüzlerinden dinle
+
